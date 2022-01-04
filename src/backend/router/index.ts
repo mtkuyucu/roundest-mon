61fb1870394cc8a2cd682d993b4365e6e224c024
Your Name <you@example.com>
2022-01-04 23:45:22 +0100

    Connected Pokemon client api and minor design chages done

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,13 @@
-import * as trpc from '@trpc/server';
-import {z} from 'zod';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { PokemonClient } from "pokenode-ts";
 
-export const appRouter = trpc.router().query('hello', {
-    input: z
-        .object({
-            text: z.string().nullish(),
-        })
-        .nullish(),
-    resolve({input}) {
-        return {
-            greeting: `hello ${input?.text ?? 'world'}`,
-        };
-    },
+export const appRouter = trpc.router().query("get-pokemon-by-id", {
+  input: z.object({ id: z.number().positive() }).nullish(),
+  resolve: async function ({ input }) {
+    const pokemonClient = new PokemonClient();
+    return pokemonClient.getPokemonById(input.id);
+  },
 });
 
 // export type definition of API
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
 import { NextPage } from "next";
 import { getOptionsForVote } from "@/utils/getRandomPokemen";
 import { useEffect, useState } from "react";
+import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
-  const [options, setOptions] = useState(false);
-  const [first, setFirst] = useState(false);
-  const [second, setSecond] = useState(false);
+  const [ids, updateIds] = useState(() => getOptionsForVote());
 
-  useEffect(() => {
-    let optionsForVote = getOptionsForVote();
-    setOptions(optionsForVote);
-    setFirst(optionsForVote[0]);
-    setSecond(optionsForVote[1]);
-  }, []);
+  const [first, second] = ids;
 
-  if (!options) {
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) {
     return <div>Loading...</div>;
   }
 
@@ -22,10 +19,28 @@ const Home: NextPage = () => {
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is roundest ?</div>
       <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl">
-        <div className="w-32 h-32 bg-red-50">{first}</div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={firstPokemon.data?.sprites.front_default}
+            alt=""
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-32 h-32 bg-red-50">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <img
+            src={secondPokemon.data?.sprites.front_default}
+            alt=""
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
+        </div>
       </div>
     </div>
   );
